1f4fcc61f7381cea946ec8214b228aa8cbf59712
Andrés Parra <pipe.parrab@gmail.com>
2024-07-23 12:13:08 -0500

    Terminado el Banner con SASS.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,21 @@
-import "../styles/index.scss"
+import "../styles/styles.scss";
+import logo from "../images/logo.svg";
+import hamburgerMenu from "../images/icon-hamburger.svg";
 
-console.log("Hola Front 8");
+document.addEventListener("DOMContentLoaded", () => {
+  const figureElement = document.querySelector("header nav figure");
+  const ulElement = document.querySelector("header ul");
+
+  const logoElement = document.createElement("img");
+  logoElement.src = logo;
+  logoElement.alt = "Logo Insure";
+
+  figureElement.appendChild(logoElement);
+
+  const hamburgerMenuElement = document.createElement("img");
+  hamburgerMenuElement.src = hamburgerMenu;
+  hamburgerMenuElement.alt = "Hamburger Menu";
+  hamburgerMenuElement.className = "header__item header__item--mobile";
+
+  ulElement.appendChild(hamburgerMenuElement);
+});
